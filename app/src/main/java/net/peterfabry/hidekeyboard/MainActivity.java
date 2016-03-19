76970848cbe9423db97bf3c3dd91518111f814e3
pero86 <pero86@gmail.com>
2016-03-19 15:33:09 +0100

    Initial commit

---
+++ app/src/main/java/net/peterfabry/hidekeyboard/MainActivity.java
@@ -0,0 +1,23 @@
+package net.peterfabry.hidekeyboard;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    void hideKeyboard(View view) {
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+    }
+}
